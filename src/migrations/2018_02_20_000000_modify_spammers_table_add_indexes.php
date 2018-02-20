cb522ebf6233104b7343b4297d7b96826d1304b8
Andrey Helldar <helldar@ai-rus.com>
2018-02-20 13:50:32 +0300

    An index was created for the IP address field

---
+++ src/migrations/2018_02_20_000000_modify_spammers_table_add_indexes.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Helldar\Spammers\migrations;
+
+use Helldar\Spammers\Traits\DbConnections;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifySpammersTableAddIndexes extends Migration
+{
+    use DbConnections;
+
+    /**
+     * ModifySpammersTableAddIndexes constructor.
+     */
+    public function __construct()
+    {
+        $this->setConnectionName();
+        $this->setTableName();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection($this->connection)
+            ->table($this->table, function (Blueprint $table) {
+                $table->dropIndex('ip_index');
+            });
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection($this->connection)
+            ->table($this->table, function (Blueprint $table) {
+                $table->ipAddress('ip');
+                $table->index('ip', 'ip_index');
+            });
+    }
+}
